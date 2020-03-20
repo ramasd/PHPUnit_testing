ae4b862689f2cbad537507f3b930e7b844a2f7b0
ramasd <ramunas.danupas@gmail.com>
2020-03-21 00:06:58 +0200

    Real project "Product List": Testing that table is (not) empty

---
+++ app/Http/Controllers/ProductController.php
@@ -0,0 +1,103 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Product;
+use Illuminate\Http\Request;
+
+class ProductController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $products = Product::all();
+
+        return view('products.index', compact('products'));
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        return view('product.ceate');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $product = new Product();
+
+        $product->name = $request->name;
+        $product->price = $request->price;
+
+        return $product->save();
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        $product = Product::find($id);
+
+        return $product;
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        $product = Product::find($id);
+
+        return view('products.edit', compact($product));
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        $product = Product::find($id);
+
+        $product->name = $request->name;
+        $product->price = $request->price;
+
+        return $product->save();
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        $product = Product::find($id);
+
+        return $product->delete();
+    }
+}
---
+++ tests/Feature/PrpductsTest.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace Tests\Feature;
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Tests\TestCase;
+use App\Product;
+
+class ExampleTest extends TestCase
+{
+    use RefreshDatabase;
+
+    public function test_homepage_contains_empty_products_table()
+    {
+        $response = $this->get('/');
+
+        $response->assertStatus(200);
+
+        $response->assertSee('No products found');
+    }
+
+    public function test_homepage_contains_non_empty_products_table()
+    {
+        $product = Product::create([
+            'name' => 'Test Product',
+            'price' => 99.99
+        ]);
+
+        $response = $this->get('/');
+
+        $response->assertStatus(200);
+
+        $response->assertDontSee('No products found');
+
+        $response->assertSee($product->name);
+    }
+}
